1563d83c78f0a378cec1fa0c783560bbfad4744d
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-19 15:53:19 -0230

    Combine single routes into map

--- app.py
+++ app.py
@@ -1,31 +1,12 @@
-from flask import Flask , request, send_file
+from flask import Flask , request, send_file, Request
 from flask_cors import CORS
 from PIL import Image
-from models.models import upscale, monet, gogh, picasso, dali
+from models.models import generate
 from random_image import generate_random_image_url
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/gogh", methods=['POST'])
-def _gogh():
-    return send_file(gogh(request_to_image(request)), mimetype='image/jpg')
-
-@app.route("/monet", methods=['POST'])
-def _monet():
-    return send_file(monet(request_to_image(request)), mimetype='image/jpg')
-
-@app.route("/picasso", methods=['POST'])
-def _picasso():
-    return send_file(picasso(request_to_image(request)), mimetype='image/jpg')
-
-@app.route("/dali", methods=['POST'])
-def _dali():
-    return send_file(dali(request_to_image(request)), mimetype='image/jpg')
-
-@app.route("/upscale", methods=['POST'])
-def _upscale():
-    return send_file(upscale(request_to_image(request)), mimetype='image/jpg')
 
 '''
 This route should return a url to a random images hosted from the github repo that that project is hosted from
@@ -35,20 +16,21 @@ def _random():
     url : str= generate_random_image_url() 
     return {'url': url}
 
-
-@app.route("/", methods=['GET'])
-def hello():
-    return "Hello World"
-
 '''
 TODO: Should generalize the generate function to take in a model name and generate the image
 '''
 @app.route("/generate/<model>", methods=['POST'])
 def _generate(model):
+    return send_file(generate(request_to_image(request), model), mimetype='image/jpg')
     pass
 
 
-def request_to_image(request):
+@app.route("/", methods=['GET'])
+def hello():
+    return "Hello World"
+
+
+def request_to_image(request: Request):
     image = request.files.get('image', False)
     print(image)
     if image:
--- models/models.py
+++ models/models.py
@@ -2,37 +2,24 @@ from tensorflow.python.ops.numpy_ops import np_config
 np_config.enable_numpy_behavior()
 
 from models.Monet import MonetGenerator
-from models.Upscale import UpscaleGenerator
 from models.Gogh import GoghGenerator
 from models.Picasso import PicassoGenerator
 from models.Dali import DaliGenerator
 
-upscale_generator = UpscaleGenerator()
-monet_generator = MonetGenerator()
-gogh_generator = GoghGenerator()
-picasso_generator = PicassoGenerator()
-dali_generator = DaliGenerator()
+
+generators = {
+    "monet": MonetGenerator(),
+    "gogh": GoghGenerator(),
+    "picasso": PicassoGenerator(),
+    "dali": DaliGenerator()
+}
 
 def load_models():
-    upscale_generator.load_model()
-    monet_generator.load_model()
-    gogh_generator.load_model()
-    picasso_generator.load_model()
-    dali_generator.load_model()
+    for generator in generators.values():
+        generator.load_model()
 
-def upscale(image):
-    return upscale_generator.generate(image)
 
-def monet(image):
-    return monet_generator.generate(image)
-
-def gogh(image):
-    return gogh_generator.generate(image)
-
-def picasso(image):
-    return picasso_generator.generate(image)
-
-def dali(image):
-    return dali_generator.generate(image)
+def generate(image, style):
+    return generators[style].generate(image)
 
 load_models()
