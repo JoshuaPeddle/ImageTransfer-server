b946d5c4ca3f88ba1bb61830951a38cd673646b2
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-25 11:26:38 -0230

    Actions is picky about import order

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,10 @@
 
 import pytest
 try:
-    from ..app import app as flask_app
-except ImportError:
     from app import app as flask_app
+   
+except ImportError:
+     from ..app import app as flask_app
 
 @pytest.fixture()
 def app():
