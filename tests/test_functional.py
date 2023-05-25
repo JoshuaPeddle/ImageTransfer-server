3c4373d6d9c2db81265c7a15d5ac2a17a56ba296
Joshua Peddle <joshuapeddle128@gmail.com>
2023-05-25 00:52:16 -0230

    Fix stlye issue

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -3,7 +3,8 @@
 def test_random_image(client):
     response = client.get("/random")
     assert response.status_code == 200
-    assert b"https://raw.githubusercontent.com/JoshuaPeddle/ImageTransfer-server/master/random_images" in response.data
+    assert b"https://raw.githubusercontent.com/JoshuaPeddle/\
+ImageTransfer-server/master/random_images" in response.data
     assert b".jpg" in response.data
 
 def test_styles(client):
